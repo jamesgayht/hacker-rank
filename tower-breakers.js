5d7903a4e961f536d52a484bc69ec443666d1f6d
James Gay <jamesgayht@gmail.com>
2023-03-30 13:34:01 +0800

    completed tower-breakers

---
+++ tower-breakers.js
@@ -0,0 +1,11 @@
+function towerBreakers(n, m) {
+    // Write your code here
+    // calculate total number of moves 
+    
+    if(n % 2 === 0 || m === 1) {
+        return 2
+    }
+    else {
+        return 1
+    }
+}
